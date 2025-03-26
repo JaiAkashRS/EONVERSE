a06fdead55af9c7bf06287a8115e4937dee3f761
JaiAkashRS <jaiakashrs06@gmail.com>
2025-03-26 19:42:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: [".js", ".jsx"], // Ensure JSX files load properly
-  },
-  esbuild: {
-    loader: "jsx", // Force JSX handling
-    include: /src\/.*\.jsx?$/, 
-  },
-  base: "./", // Fix asset paths for relative loading
+  base: "/", // Ensure assets load correctly
 });
